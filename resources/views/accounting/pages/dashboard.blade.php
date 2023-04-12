ba39d8eb1bfc292f969547e4b2adccf5c489bb56
NismaArshad <65742363+NismaArshad@users.noreply.github.com>
2023-04-12 10:50:48 -0700

    check

--- resources/views/accounting/pages/dashboard.blade.php
+++ resources/views/accounting/pages/dashboard.blade.php
@@ -47,7 +47,7 @@
             @foreach($requests as $request)
                 <tr>
                   <td>
-                    
+                    <input type="checkbox" name="id[]" value="<?php  echo $request->id ?>">
                   </td>
                     <td>{{$request->initiator ?? ''}}</td>
                     <td>{{$request->company->name ?? ''}}</td>
@@ -116,8 +116,6 @@
 
 @endsection
 @section('script')
-<link type="text/css" href="//gyrocode.github.io/jquery-datatables-checkboxes/1.2.12/css/dataTables.checkboxes.css" rel="stylesheet" />
-<script type="text/javascript" src="//gyrocode.github.io/jquery-datatables-checkboxes/1.2.12/js/dataTables.checkboxes.min.js"></script>
 
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
   
@@ -135,19 +133,11 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<link type="text/css" href="//gyrocode.github.io/jquery-datatables-checkboxes/1.2.12/css/dataTables.checkboxes.css" rel="stylesheet" />
-<script type="text/javascript" src="//gyrocode.github.io/jquery-datatables-checkboxes/1.2.12/js/dataTables.checkboxes.min.js"></script>
+
     <script>
        $(document).ready(function () {
    
             $('#accounting').DataTable({
-              'columnDefs': [
-         {
-            'targets': 0,
-            'checkboxes': true,
-         }
-      ],
-      'order': [[1, 'asc']],
               dom: 'Blfrtip',
           lengthChange: true,
           buttons: [
@@ -180,22 +170,6 @@ columns: [0,1,2,3,4, 5, 6, 7, 8,9,10,11]
         });
 
       $(document).ready(function () {
-        $('#frm-example').on('submit', function(e){
-      var form = this;
-
-      var rows_selected = table.column(0).checkboxes.selected();
-
-      // Iterate over all selected checkboxes
-      $.each(rows_selected, function(index, rowId){
-         // Create a hidden element
-         $(form).append(
-             $('<input>')
-                .attr('type', 'hidden')
-                .attr('name', 'id[]')
-                .val(rowId)
-         );
-      });
-   });
 
         $("#all").click(function(){
               var id = '1';
