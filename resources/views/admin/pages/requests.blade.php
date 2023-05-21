1973a32311be935c7953d768f94cd2b7b27b3f28
NismaArshad <65742363+NismaArshad@users.noreply.github.com>
2023-05-21 12:49:49 -0700

    ni

--- resources/views/admin/pages/requests.blade.php
+++ resources/views/admin/pages/requests.blade.php
@@ -30,9 +30,6 @@
           </form> </div>
     <!--end::Header-->
     <!--begin::Body-->
-<<<<<<< Updated upstream
-
-=======
     <div class="modal fade" id="rowModal" tabindex="-1" role="dialog" aria-labelledby="rowModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -66,7 +63,6 @@
         </div>
     </div>
   </div>
->>>>>>> Stashed changes
 
     <div class="container">
       <div class="overflow-auto">
