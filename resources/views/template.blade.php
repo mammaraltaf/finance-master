0c328d0f742599f159b36e752a41088eab8719dd
NismaArshad <65742363+NismaArshad@users.noreply.github.com>
2023-05-23 09:15:33 -0700

    final

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,9 +6,9 @@
 <body>
     <?php
     
-// echo $request['amount_in_gel'];
+// echo ( $request['typeOfExpense']);
 // echo "<br>";
-
+// exit();
 // foreach ($logs as $a) {
 //     print_r($a);
 //     echo "<br>";
@@ -19,6 +19,19 @@
 <br>
 <h4>Approval History Information</h4>
 <p>Approval Status: 1 of 1 received</p>
+<p>Initiator: <?php echo $request['initiator']; ?></p>
+<p>Company: <?php echo $request['company']['name']; ?></p>
+<p>Department: <?php echo $request['department']['name']; ?></p>
+<p>Supplier: <?php echo $request['supplier']['supplier_name']; ?></p>
+<p>Type Of Expense: <?php echo $request['typeOfExpense']['name']; ?></p>
+<p>Currency: <?php echo $request['currency']; ?></p>
+<p>Amount: <?php echo $request['amount']; ?></p>
+<p>Amount in gel: <?php echo $request['amount_in_gel']; ?></p>
+<p>Basis: <?php  echo $request['basis']; ?></p>
+<p>Due Date Payment: <?php echo $request['payment_date']; ?></p>
+<p>Due Date Submission: <?php echo $request['submission_date']; ?></p>
+<p>Description: <?php echo $request['description']; ?></p>
+<p>Status: <?php echo $request['status']; ?></p>
 
     <table class="table">
     <thead>
@@ -40,11 +53,7 @@
         @endforeach
     </tbody>
     </table>
-    
-        <p>Initiator: <?php echo $request['initiator']; ?></p>
-        <p>Amount in gel: <?php echo $request['amount_in_gel']; ?></p>
-        <p>Status: <?php echo $request['status']; ?></p>
-        
+    <?php //exit(); ?>
        
 </body>
 </html>
