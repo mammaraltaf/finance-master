0a55ac02f2224716cf6b259d3593b11374774850
imzaraf <imafaraz18@gmail.com>
2023-07-16 09:39:02 +0500

    Sorting Resolved

--- resources/views/logs/logging.blade.php
+++ resources/views/logs/logging.blade.php
@@ -306,8 +306,40 @@
             });
         });
 
+        // sort by date
+        jQuery.extend(jQuery.fn.dataTableExt.oSort, {
+            "date-uk-pre": function (a) {
+                var ukDatea = a.split('/');
+                return (ukDatea[2] + ukDatea[1] + ukDatea[0]) * 1;
+            },
+
+            "date-uk-asc": function (a, b) {
+                return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+            },
+
+            "date-uk-desc": function (a, b) {
+                return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+            }
+        });
         $(document).ready(function() {
             $('#reviewDocument').DataTable({
+                "aoColumns": [
+                    null,
+                    null,
+                    {"sType": "date-uk"},
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    {"sType": "date-uk"},
+                    {"sType": "date-uk"},
+                ],
                 'order':[[3,'desc']],
                 dom: 'Blfrtip',
                 lengthChange: true,
